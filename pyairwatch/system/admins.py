b722b6bf3740d7e0771109444322894798a94fb4
John Richards <johnprichards3@gmail.com>
2018-03-19 22:10:05 -0400

    added ability get user or admin id from username

--- pyairwatch/system/admins.py
+++ pyairwatch/system/admins.py
@@ -20,6 +20,23 @@ class Admins(object):
         response = self._get(path='/admins/search', params=kwargs)
         return response
 
+    def get_id_from_username(self, username):
+        """Returns the Admin User ID for a given Username"""
+        userID = ''
+        response = self.search(username=str(username))
+        # search does not do an exact string match, so we need to do it here
+        if len(response['Admins']) > 1:
+            for u in response['Admins']:
+                if username == u['UserName']:
+                    #If there are multiple admins with the same exact username it picks the first one it comes across
+                    userID = u['Id']['Value']
+                    break
+                else:
+                    continue
+        else:
+            userID = response['Admins'][0]['Id']['Value']
+        return userID
+
     def _get(self, module='system', path=None, version=None, params=None, header=None):
         """GET requests for the /System/Admins module."""
         response = self.client.get(module=module, path=path, version=version, params=params, header=header)
