d7c9a6848084fd7869d8cea5a77524c2a23c33da
Jacob Postema <jpostema@vmware.com>
2018-09-23 11:16:23 -0400

    creating api end point class structure

--- pyairwatch/system/featureflag.py
+++ pyairwatch/system/featureflag.py
@@ -1,27 +1,24 @@
-class FeatureFlag(object):
+from .system import System
+
+
+class FeatureFlag(System):
     """
     For Feature Flags
     """
 
     def __init__(self, client):
-        self.client = client
+        super().__init__(client)
 
     def set_flag(self, feature_flag, og_uuid, override):
         """Set the Feature Flag"""
-        return self._post(path='/featureFlag/{}/{}/{}'.format(feature_flag, og_uuid, override))
+        path = '/featureFlag/{}/{}/{}'.format(feature_flag, og_uuid, override)
+        return super._post(path=path)
 
     def get_status(self, feature_flag, og_uuid):
         """GET a specific Feature Flag status"""
-        return self._get(path='/featureFlag/{}/{}'.format(feature_flag, og_uuid))
+        path = '/featureFlag/{}/{}'.format(feature_flag, og_uuid)
+        return super._get(path=path)
 
     def og_status(self, og_uuid):
         """GET all Feature Flags for a particular OG need UUID"""
-        return self._get(path='/featureFlag/{}'.format(og_uuid))
-
-    def _get(self, module='system', path=None, version=None, params=None, header=None):
-        """GET requests for the /System/featureFlag module."""
-        return self.client.get(module=module, path=path, version=version, params=params, header=header)
-
-    def _post(self, module='system', path=None, version=None, params=None, data=None, json=None, header=None):
-        """POST requests for the /System/featureFlag module."""
-        return self.client.post(module=module, path=path, version=version, params=params, data=data, json=json, header=header)
+        return super._get(path='/featureFlag/{}'.format(og_uuid))
