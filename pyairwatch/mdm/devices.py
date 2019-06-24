ed3ec82e7c4f7110ff9af8374248778261c9a463
Jacob Postema <jpostema@vmware.com>
2019-06-24 11:33:24 -0400

    - Fixed a bug with search function - renamed a function to be more consistant - reworked some string manipulation ot be consistant - added 3 function for getting security infor sample info - added function to retrevie jsut FIleVault Key

--- pyairwatch/mdm/devices.py
+++ pyairwatch/mdm/devices.py
@@ -11,7 +11,7 @@ class Devices(MDM):
 
     def search(self, **kwargs):
         """Returns the Device information matching the search parameters."""
-        return MDM._get(path='/devices', params=kwargs)
+        return MDM._get(self, path='/devices', params=kwargs)
 
     def get_details_by_alt_id(self, serialnumber=None, macaddress=None, udid=None, imeinumber=None, easid=None):
         """Returns the Device information matching the search parameters."""
@@ -51,12 +51,12 @@ class Devices(MDM):
         """
         return MDM._post(self, path='/devices/{}/clearpasscode'.format(device_id))
 
-    def commands_for_device_id(self, command, device_id):
+    def send_commands_for_device_id(self, command, device_id):
         """
         Commands for devices selecting device based on id
         """
         path = '/devices/{}/commands'.format(device_id)
-        command = 'command=' + command
+        command = 'command={}'.format(command)
         return MDM._post(self, path=path, params=command)
 
     def send_commands_by_id(self, command, searchby, id):
@@ -64,8 +64,9 @@ class Devices(MDM):
         Commands for devices selecting device based on id
         """
         path = '/devices/commands'
-        query = 'command=' + str(command) + '&searchBy=' + str(searchby)
-        query = query + '&id=' + str(id)
+        query = 'command={}&searchBy={}&id={}'.format(str(command),
+                                                      str(searchby),
+                                                      str(id))
         return MDM._post(self, path=path, params=query)
 
     def get_details_by_device_id(self, device_id):
@@ -73,3 +74,38 @@ class Devices(MDM):
         device detals by device id
         """
         return MDM._get(self, path='/devices/{}'.format(device_id))
+
+    def get_device_filevualt_recovery_key(self, device_uuid):
+        """
+        Gets a maccOS device's FileVualt Recovery Key
+        """
+        _path = '/devices/{}/security/recovery-key'.format(device_uuid)
+        return MDM._get(self, path=_path)
+
+    def get_security_info_by_id(self, device_id):
+        """
+        Processes the device ID to retrieve the security
+        information sample related info
+        """
+        _path = '/devices/{}/security'.format(device_id)
+        return MDM._get(self, path=_path)
+
+    def get_security_info_by_atlternat_id(self, searchby, id):
+        """
+        Processes the device ID to retrieve the security
+        information sample related info by Alternater ID
+        """
+        _path = '/devices/security'
+        _params = 'searchby={}&id={}'.format(searchby, id)
+        return MDM._get(self, path=_path, params=_params)
+
+    def get_bulk_security_info(self, organization_group_id, user_name, params=None):
+        """
+        Processes the information like organizationgroup ID, user name, model,
+        platform, last seen, ownership, compliant status, seen since parameters
+        and fetches the security information for the same.
+        """
+        _path = '/devices/securityinfosearch'
+        _query = 'organizationgroupid={}&user={}'.format(organization_group_id,
+                                                         user_name)
+        return MDM._get(self, path=_path, params=_query)
