7466d10570ef29fe58e282a5ce94580d92938bfe
Jacob Postema <jpostema@vmware.com>
2019-09-19 10:54:45 -0400

    Added Tests and a template for environment as I don't want to expose any environment details.

---
+++ tests/testing_is_important.py
@@ -0,0 +1,26 @@
+from pyairwatch.client import AirWatchAPI
+import unittest
+import json
+
+
+class TestBasicTests(unittest.TestCase):
+    def setUp(self):
+        input = "cnaapp"
+        with open("enviroments.json") as json_file:
+            server_details = json.load(json_file)
+        self.enviroment = AirWatchAPI(env=server_details[input]["server"],
+                                      apikey=server_details[input]["apikey"],
+                                      username=server_details[input]["username"],
+                                      password=server_details[input]["password"])
+
+    def teardown(self):
+        print("TEAR DOWN!")
+
+    def test_basic(self):
+        response = self.enviroment.info.get_enviroment_info()
+        self.assertEqual(response["ProductName"], "AirWatch Platform Service",
+                         "Info API ran")
+
+
+if __name__ == '__main__':
+    unittest.main()
